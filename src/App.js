788064a19f1ef6fc6eab81939785d427fb3de002
ashwin-athappan <ashwinathappank@gmail.com>
2022-10-23 11:11:14 +0530

    Added feature to App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is your React App No No
+          This is your React App In Feature Update
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React In Feature
         </a>
       </header>
     </div>
